4bfaace1cdeb16f0ef7df6be09a8d033c4f5da48
Double Do <jimmyangz@yahoo.com>
2017-08-10 22:02:53 +0900

    Update data_batch.py

--- data_batch.py
+++ data_batch.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
+
 import os
 import numpy as np
 import Config
 import random
 conf = Config.config()
-
 
 def create_data(data_dir, random_dir):
     with open(random_dir, 'r') as f_random:
@@ -23,8 +23,6 @@ def create_data(data_dir, random_dir):
     
     return docs, sums
 
-
-
 def vocab_build(train_doc, vocab_size):
     data=[]
     for doc in train_doc:
@@ -38,9 +36,6 @@ def vocab_build(train_doc, vocab_size):
         vocab.append(word_freq[0])
     
     return vocab
-    
-
-
 
 def data_generation():
     pub_dir = '/Users/zy/Desktop/'
@@ -62,14 +57,11 @@ def data_generation():
     vocab.append('<go>')
     vocab.append('<eos>')
     vocab.append('<pad>')
-    
     return (train_doc, train_sum), (val_doc, val_sum), (test_doc, test_sum), vocab
-
 
 def np_int(data):
     return np.array(data).astype('int32')    
 
-    
 def word2id(docu, summ, vocab, is_training=False):
     vocab_set = set(vocab)
     docu1=[]
@@ -89,8 +81,7 @@ def word2id(docu, summ, vocab, is_training=False):
             else:
                 d1.append(vocab.index('<unk>'))
         docu1.append(d1)
-    
-    
+   
     if is_training:
         y_true_labels=[]
         deco_inputs=[]
@@ -124,19 +115,15 @@ def word2id(docu, summ, vocab, is_training=False):
             y_true_labels.append(y_true1)    
             deco_inputs.append(deco_input1)                    
         
-        return np_int(docu1), np_int(deco_inputs), np_int(y_true_labels), docs_mask, summ_mask
-    
-   
-  
+        return np_int(docu1), np_int(deco_inputs), np_int(y_true_labels), docs_mask, summ_mask    
+
 def batch_shuffle(train_doc2id, train_y_true2id, train_deco_inputs2id, doc_mask, sum_mask):
     l = len(train_doc2id)
     data_shuffle=[]
     for i in range(l):
         data_shuffle.append([train_doc2id[i], train_y_true2id[i], train_deco_inputs2id[i], doc_mask[i], sum_mask[i]])
-    
-    
+     
     random.shuffle(data_shuffle)
-    
     train_doc2id1=[]
     train_y_true2id1=[]
     train_deco_inputs2id1=[]
@@ -156,44 +143,16 @@ def data_batch():
     print "data_batch..."
     (train_doc, train_sum), (val_doc, val_sum), (test_doc, test_sum), vocab = data_generation()
     print "finished!"
-       
-    
     print "convert to id..."           
     train_doc2id, train_deco_inputs2id, train_y_true2id, doc_mask, sum_mask = word2id(train_doc, train_sum, vocab, is_training=True)
     #val_doc2id, val_sum2id = word2id(val_doc, val_sum, vocab)
     #test_doc2id, test_sum2id = word2id(test_doc, test_sum, vocab)
     print "finished!"  
-    
     print "shuffle..."        
     train_doc2id, train_y_true2id, train_deco_inputs2id, doc_mask, sum_mask = batch_shuffle(train_doc2id, train_y_true2id, train_deco_inputs2id, doc_mask, sum_mask)
     print "finished!"
-    
     return train_doc2id, train_y_true2id, train_deco_inputs2id, doc_mask, sum_mask, vocab
-   
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
     
